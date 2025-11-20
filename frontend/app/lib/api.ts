5652362e4c2078377cc7e5c17dbaad0c69c0e25b
Jivansh Sharma <jivansh17@gmail.com>
2025-11-20 18:41:46 +0530

    💚 prefix next env

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { InjectOrderType } from "./types";
 
-const API_URL = process.env.BACKEND_URL || "http://localhost:9000/api";
+const API_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:9000/api";
 
 export const api = axios.create({
   baseURL: API_URL,
